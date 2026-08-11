9260b2ad89f69992a44cd69826cc96f2797de337
Amin Azimi <aminazimi149@gmail.com>
2026-08-12 02:15:14 +0330

    edit env file config from both docker-compose.yml and settings.py side

--- Backend/CarExpress/settings.py
+++ Backend/CarExpress/settings.py
@@ -12,19 +12,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
-from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-env_file = BASE_DIR.parent.parent / ".env"
-load_dotenv(env_file, override=False)
 
-if env_file.exists():
-    print("var")
-else:
-    print("yox")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
