e3c7e0e26bd45fbef9b5a12b848cb9405789c90b
Amin Azimi <aminazimi149@gmail.com>
2026-08-16 13:17:12 +0330

    add urls

--- Backend/accounts/urls.py
+++ Backend/accounts/urls.py
@@ -1,5 +1,14 @@
+# accounts/urls.py
+
 from django.urls import path
+from . import views  # You will create these views next
+
+app_name = "accounts"
 
 urlpatterns = [
-
+    # Corresponds to: POST /accounts/auth/otp/generate/
+    path("auth/otp/generate/", views.GenerateOTPView.as_view(), name="generate-otp"),
+    
+    # Corresponds to: POST /accounts/auth/otp/verify/
+    path("auth/otp/verify/", views.VerifyOTPView.as_view(), name="verify-otp"),
 ]
