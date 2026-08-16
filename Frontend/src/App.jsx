cdb60ac14209db67f29dd6b07b7c1ae37b988304
MoeinX <moein.niromand86@gmail.com>
2026-08-16 23:48:52 +0330

    Edit App.jsx And Add New Pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
-import ContactPage from './pages/ContactPage';
-import AuthPage from './pages/AuthPage';
-import RequestWizard from './pages/RequestWizard';
 import TrackingPage from './pages/TrackingPage';
-import PaymentPage from './pages/PaymentPage';
-import UserDashboard from './pages/UserDashboard';
 
 export default function App() {
   return (
@@ -15,17 +9,7 @@ export default function App() {
       <Routes>
         {/* صفحات اصلی و عمومی */}
         <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        
-        {/* صفحه احراز هویت (شامل ورود، ثبت‌نام و OTP در یک کامپوننت) */}
-        <Route path="/auth" element={<AuthPage />} />
-        
-        {/* صفحات مرتبط با پنل کاربر و عملیات حمل و نقل */}
-        <Route path="/dashboard" element={<UserDashboard />} />
-        <Route path="/request" element={<RequestWizard />} />
         <Route path="/tracking" element={<TrackingPage />} />
-        <Route path="/payment" element={<PaymentPage />} />
       </Routes>
     </Router>
   );
